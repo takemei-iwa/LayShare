7d48114d71e198e9d4b9c091fc1ade4ca6a060cd
takemei-iwa <takemei567@gmail.com>
2024-09-14 17:54:12 +0900

    レイアウト作成画面のreact-ver

---
+++ resources/js/Pages/Layouts/Create.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Link } from '@inertiajs/react';
+import html2canvas from 'html2canvas';
+
+export default function Create() {
+    const [html, setHtml] = useState('');
+    const [css, setCss  ] = useState('');
+
+    const iframeRef = useRef(null);
+
+    const iframeTryCatch = (func) => {
+        // iframe要素の取得
+        const iframe = iframeRef.current;
+        // iframeとそのcontentDocumentがnull出ない場合に実行
+        try {
+            if (!iframe) {
+                throw new Error('Iframe element not found');
+              }
+            if (!iframe.contentDocument) {
+                throw new Error('Iframe contentDocument not accessible');
+            }
+            func(iframe);
+        } catch (error) {
+            console.error("iframe error : ", error.message);
+        }
+    }
+    // iframeに記述したhtmlとcssの変更を反映
+    useEffect(() => {
+        iframeTryCatch((iframe) => {
+            const iframeDoc = iframe.contentDocument;
+            iframeDoc.open();
+            iframeDoc.write(`
+                      <style>${css}</style>
+                      ${html}
+                  `);
+            iframeDoc.close();
+            }
+        );
+    }, [html, css]);
+      
+    // 送信用関数を追加
+    const handleSendPosts = (e) => {
+        e.preventDefault(); 
+        iframeTryCatch((iframe) => {
+            const iframeDoc = iframe.contentDocument;
+            let imageUrl = "";
+            // Make sure the iframe's content is fully loaded before capturing
+            html2canvas(iframeDoc.body).then(function (canvas) {
+                const img = canvas.toDataURL('image/png');
+                imageUrl = img;
+                const formData = {
+                    image: imageUrl, 
+                    html: html,
+                    css: css,                
+                }
+                fetch('/layouts/create', {
+                    method: 'POST', // HTTPメソッドを指定
+                    headers: {
+                        'Content-Type': 'application/json', // リクエストの内容タイプを指定
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                    },
+                    body: formData // 送信するデータをJSON形式に変換
+                  })
+            }).catch(function (error) {
+                console.error('Error capturing the screenshot:', error);
+            });
+        })
+        
+    }
+    return (
+        <div>    
+            {/* <form id="form" action="{{ route('layouts.store') }}" method="POST"> */}
+            <form onSubmit={handleSendPosts}>                
+                <input type="text" name="image" id="image" value="imagedesu"/>
+                <div>
+                    <textarea id="html-editor" name="html" 
+                    placeholder="Enter HTML here..." 
+                    onChange={(e) => setHtml(e.target.value) }>
+                    </textarea>
+                    <textarea id="css-editor" name="css" 
+                    placeholder="Enter CSS here..." 
+                    onChange={(e) => setCss(e.target.value)}></textarea>
+                </div>
+                <button type="submit">投稿</button>                
+            </form>
+            <iframe id="preview" ref={iframeRef}></iframe>
+        
+            <div class="footer">
+                <Link href="/layouts">戻る</Link>
+            </div>
+        </div>
+    );
+}
+
