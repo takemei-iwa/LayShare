7fbc0f873bb1906440b38d50358859f7d5bd4699
takemei-iwa <takemei567@gmail.com>
2024-09-23 15:37:46 +0900

    非同期でいいね機能の実装

--- resources/js/Pages/Layouts/Edit.jsx
+++ resources/js/Pages/Layouts/Edit.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { router } from '@inertiajs/react'
+import axios from 'axios';
 import MainLayout from "@/Layouts/MainLayout";
 
 import Editor from "../../Components/Layouts/Editor";
@@ -24,16 +25,41 @@ export default function Edit(props) {
             router.post("/layouts/create", data);
         }                
     }
-
-    const handleSendLike = (e) => {
-        e.preventDefault(); 
-        if(like){
-            router.delete(`/layouts/${layout.id}/like`);
-        } else {
-            router.post(`/layouts/${layout.id}/like`);
+    const sendLikeRequest = async (method, url) => {
+        try {
+            if (method === 'delete') {
+                await axios.delete(url);
+            } else if (method === 'post') {
+                await axios.post(url);
+            }
+            return true;
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                router.get(route("login"));
+            } else {
+                console.log("Error: ", error);
+            }
+            return false;
         }
-        router.get(`/layouts/${layout.id}`);
-    }
+    };
+    
+    const handleSendLike = async (e) => {
+        e.preventDefault();
+        const url = `/layouts/${layout.id}/like`;
+    
+        if (like) {
+            const success = await sendLikeRequest('delete', url);
+            if (success){
+                setLike(false);
+            }
+        } else {
+            const success = await sendLikeRequest('post', url);
+            if (success){
+                setLike(true);
+            }
+        }
+    };
+    
     return (
         <MainLayout user={props.auth.user}>
             <button type="submit" onClick={handleSendPosts}>保存</button>   
