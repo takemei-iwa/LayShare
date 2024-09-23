c00f23e634835b82ccdca293e6ad925972af3ca2
takemei-iwa <takemei567@gmail.com>
2024-09-23 21:48:06 +0900

    一覧表示コンポーネントを適用

--- resources/js/Pages/Layouts/Index.jsx
+++ resources/js/Pages/Layouts/Index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from '@inertiajs/react';
-import { router } from '@inertiajs/react';
 import MainLayout from "@/Layouts/MainLayout";
+import Show from "@/Components/Layouts/Show";
 
 export default function Index(props) {
     const { layouts } = props;
@@ -10,21 +10,7 @@ export default function Index(props) {
 
     return (
         <MainLayout user={props.auth.user}>
-            <h1> layouts</h1>
-            <p>{layouts[0].id}</p> 
-            <div class="grid grid-cols-3 gap-2">
-            { layouts.map((layout) => (     
-                    <div key={layout.id}>
-                        <div class="w-full aspect-w-9 aspect-h-5 overflow-hidden bg-white">
-                        <img class="object-cover w-full h-auto object-left-top" alt=""
-                            src = {layout.thumbnail} 
-                            onClick = {() => router.get(`/layouts/${layout.id}`) }/>
-                            </div>
-                    </div>
-                )) }
-            </div>
-            
-            <Link href="/layouts/create">レイアウトの投稿</Link>
+            <Show layouts={layouts} />
         </MainLayout>
     );
 }
