b8d659599b761cba6cd3e27cf3f0bce161282add
takemei-iwa <takemei567@gmail.com>
2024-09-23 11:24:57 +0900

    余白部分の背景を白にして、画像のサイズにかかわらず均一に表示

--- resources/js/Pages/Layouts/Index.jsx
+++ resources/js/Pages/Layouts/Index.jsx
@@ -15,7 +15,7 @@ export default function Index(props) {
             <div class="grid grid-cols-3 gap-2">
             { layouts.map((layout) => (     
                     <div key={layout.id}>
-                        <div class="w-full aspect-w-9 aspect-h-5 overflow-hidden">
+                        <div class="w-full aspect-w-9 aspect-h-5 overflow-hidden bg-white">
                         <img class="object-cover w-full h-auto object-left-top" alt=""
                             src = {layout.thumbnail} 
                             onClick = {() => router.get(`/layouts/${layout.id}`) }/>
