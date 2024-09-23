6b2a0ebfc0bcc3f6dafd89c4d3c87ec2674fa745
takemei-iwa <takemei567@gmail.com>
2024-09-23 21:46:43 +0900

    レイアウト一覧表示するコンポーネントの作成

---
+++ resources/js/Components/Layouts/Show.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Show({ layouts }) {
+    return (
+        <div class="grid grid-cols-3 gap-2">
+            {layouts.map((layout) => (
+                <div key={layout.id}>
+                    <div class="w-full aspect-w-9 aspect-h-5 overflow-hidden bg-white">
+                        <img class="object-cover w-full h-auto object-left-top" alt=""
+                            src={layout.thumbnail}
+                            onClick={() => router.get(`/layouts/${layout.id}`)} />
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
