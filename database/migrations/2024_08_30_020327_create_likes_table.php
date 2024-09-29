964d00a2332a82dd47b77c319603b2316e5dc914
takemei-iwa <takemei567@gmail.com>
2024-09-29 12:16:17 +0900

    likesテーブルの複合主キーの削除

--- database/migrations/2024_08_30_020327_create_likes_table.php
+++ database/migrations/2024_08_30_020327_create_likes_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('likes', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
